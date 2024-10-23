8100b7e0b69db27967fb3d8fde01e6435197f19c
David Enke <david@enke.dev>
2024-10-23 11:37:28 +0200

    feat(xlp-icon-button): allow setting badges
    for text labels

--- src/components/icon-button/icon-button.component.ts
+++ src/components/icon-button/icon-button.component.ts
@@ -1,5 +1,8 @@
+import '../badge/badge.component.js';
+
 import { html, LitElement, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { when } from 'lit/directives/when.js';
 
 import styles from './icon-button.component.css?inline';
 
@@ -10,8 +13,12 @@ export class IconButton extends LitElement {
   @property({ type: Boolean, reflect: true })
   disabled = false;
 
+  @property({ type: String, reflect: true })
+  badge?: string;
+
   override render() {
     return html`
+      ${when(this.badge !== undefined, () => html`<xlp-badge>${this.badge}</xlp-badge>`)}
       <button ?disabled="${this.disabled}">
         <slot></slot>
       </button>
