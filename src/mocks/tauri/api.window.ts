e319392a6ddc001871c490e10f3a93fd4fca59a6
David Enke <david@enke.dev>
2024-10-25 21:40:40 +0200

    chore: add types for the window api

--- src/mocks/tauri/api.window.ts
+++ src/mocks/tauri/api.window.ts
@@ -1,12 +1,20 @@
+import type * as ApiWindow from '@tauri-apps/api/window';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export class LogicalSize {
+export class LogicalSize implements ApiWindow.LogicalSize {
+  public type = 'LogicalSize';
+
   constructor(
     public width: number,
     public height: number,
   ) {}
+
+  toPhysical() {
+    return {} as ApiWindow.PhysicalSize;
+  }
 }
 
-export function getCurrentWindow() {
+export const getCurrentWindow: typeof ApiWindow.getCurrentWindow = () => {
   return {
     onCloseRequested: (...args: any[]) => console.info('tauri:', 'onCloseRequested', args),
     setResizable: (...args: any[]) => console.info('tauri:', 'setResizable', args),
@@ -14,5 +22,5 @@ export function getCurrentWindow() {
     setTitle: (...args: any[]) => console.info('tauri:', 'setTitle', args),
     setTitleBarStyle: (...args: any[]) => console.info('tauri:', 'setTitleBarStyle', args),
     startDragging: (...args: any[]) => console.info('tauri:', 'startDragging', args),
-  };
-}
+  } as ApiWindow.Window;
+};
